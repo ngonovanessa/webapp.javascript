a3ac26f0c52f2fb969547ddb433d86e62fe84243
Angelvicks <angetaffo19@gmail.com>
2022-08-07 09:32:15 +0100

    modern calculator

---
+++ assets/scripts/app.js
@@ -0,0 +1,82 @@
+// GLOBAL SCOPE
+let currentResult = 0;
+let logResult = [];
+
+
+// Displays calculation description
+function getDescription(temporal, operator) {
+  return `${temporal} ${operator} ${getUserInput()}`;
+}
+function calculate(type) {
+
+  let temporal = currentResult;
+  if (
+    type != "ADD" &&
+    type != "SUBTRACT" &&
+    type != "MULTIPLY" &&
+    type != "DIVIDE" 
+  ) {
+  
+    return;
+  }
+ 
+  let operator;
+ 
+  if (type == "ADD") {
+
+    currentResult += getUserInput();
+    // currentResult = 6
+    operator = "+";
+  } else if (type == "SUBTRACT") {
+    currentResult -= getUserInput();
+    operator = "-";
+  } else if (type == "MULTIPLY") {
+    currentResult *= getUserInput();
+    operator = "*";
+  } else if (type == "DIVIDE") {
+    currentResult /= getUserInput();
+    operator = "/";
+  } 
+
+  const calcDescription = getDescription(temporal, operator);
+  writeLog(currentResult, calcDescription);
+  outputResult(currentResult, calcDescription);
+}
+
+
+// Get's user input.
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
+
+function writeLog(currentResult, calcDescription) {
+  let resultObj = {
+    result: currentResult,
+    description: calcDescription,
+  };
+  logResult.push(resultObj);
+  console.log(logResult);
+}
+
+function add() {
+  calculate("ADD");
+}
+
+function subtract() {
+  calculate("SUBTRACT");
+}
+
+function multiply() {
+  calculate("MULTIPLY");
+}
+
+function divide() {
+  calculate("DATAGIRL");
+}
+
+subtractBtn.addEventListener("click", subtract);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+addBtn.addEventListener("click", add);
---
+++ assets/scripts/co.js
@@ -0,0 +1,62 @@
+  
+let currentResult = 0
+let logResult = []
+
+ 
+// Get's user input.
+function getUserInput(){
+   return parseInt(userInput.value)
+}
+
+// Displays calculation description
+function getDescription(operator){
+   return `${currentResult} ${operator} ${getUserInput()}`
+}
+
+
+
+function add(){ 
+  const calcDescription = getDescription('+');
+  currentResult +=  getUserInput();
+  let resultObj = {
+    result: currentResult,
+    
+  }
+  logResult.push(resultObj)
+  console.log(logResult)
+  outputResult(currentResult, calcDescription)
+}
+
+
+addBtn.addEventListener('click', add)
+
+function subtract(){
+    const calcDescription = getDescription('-')
+    currentResult -= getUserInput();
+    outputResult(currentResult, calcDescription)
+}
+
+subtractBtn.addEventListener('click',subtract)
+
+
+function multiply(){
+    const calcDescription = getDescription('*')
+    currentResult *= getUserInput();
+    outputResult(currentResult, calcDescription)
+}
+
+multiplyBtn.addEventListener('click', multiply)
+
+function divide(){
+    const calcDescription = getDescription('/')
+    currentResult /= getUserInput();
+    outputResult(currentResult, calcDescription)
+}
+
+divideBtn.addEventListener('click', divide);
+
+
+// Numbers
+// Strings
+// Boolean 
+// Arrays 
